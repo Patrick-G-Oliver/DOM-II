8b8cc366851d00dda4aac93f0464bbc5b8567761
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-04-04 19:22:54 -0400

    all tasks completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,77 @@
 // Your code goes here
+
+//////////////// the h1 ////////////////////////
+const funBusLogo = document.querySelector('h1');
+// 1
+funBusLogo.addEventListener('click', (event) => {
+    event.target.style.color = 'red';
+});
+// 2
+funBusLogo.addEventListener('dblclick', (event) => {
+    event.target.style.color = 'green';
+});
+////////////////////////////////////////////////
+
+//////////////// intro header ///////////////////////
+const welcomeHeader = document.querySelector('.intro');
+// 3
+welcomeHeader.addEventListener('mouseenter', (event) => {
+    event.target.style.color = 'blue';
+});
+// 4
+welcomeHeader.addEventListener('mouseleave', (event) => {
+    event.target.style.color = 'black';
+});
+// 5
+welcomeHeader.addEventListener('copy', (event) => {
+    event.target.style.backgroundColor = 'purple';
+});
+//////////////////////////////////////////////////////
+
+///////////////// nav bar ////////////////////////////
+const navItems = document.querySelector('nav');
+// 6
+navItems.addEventListener('mousemove', (event) => {
+    event.target.style.color = 'green';
+});
+// 7
+navItems.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'black';
+});
+// 8
+navItems.addEventListener('mousedown', (event) => {
+    event.target.style.color = 'yellow';
+});
+// 9
+navItems.addEventListener('mouseup', (event) => {
+    event.target.style.color = 'orange';
+});
+//////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////
+
+////////// .stopPropagation() on Destinations Section ///////
+const destino = document.getElementsByClassName('destination');
+
+destino[0].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'yellow';
+});
+// 10
+destino[0].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green';
+});
+
+const sunButton = document.getElementsByClassName('btn');
+
+sunButton[0].addEventListener('click', (event) => {
+    event.stopPropagation();
+})
+/////////////////////////////////////////////////////////////
+
+//////// .preventDefault() on nav items /////////////////////
+navItems.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
